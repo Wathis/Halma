d8aec66a6d866c6960510be7df8ef80e2c2e2b71
Mathis Delaunay <delaunaymathis@yahoo.fr>
2016-12-05 08:10:09 +0100

    Repeat selection

--- mouvements.c
+++ mouvements.c
@@ -11,22 +11,25 @@ int deplacerUnPion(Plateau *plateau){ //x et y sont les coordonnées où déplac
         selectionnerUnPion(&caseSelectionne,joueur);
     }while (verificationDeSelection(plateau,&caseSelectionne,joueur));
 
-	//Rafraichir l'écran de jeu
-    system("clear");
+    //Rafraichir l'écran de jeu
     afficherLePlateau(plateau);
-	//On lui demande les coordonées où il veut aller
-    selectionnerUneCaseDeplacement(&caseDeplacement,joueur);
 
-    //Ici il y aura la vérification 
-    if (verificationDeDeplacement(plateau,caseSelectionne,caseDeplacement) == 1){
-        //On déplace le joueur 
-        plateau->tab[caseSelectionne.y - 1][caseSelectionne.x - 1] = 0;
-        plateau->tab[caseDeplacement.y - 1][caseDeplacement.x - 1] = joueur;
-        afficherLePlateau(plateau);
-    }else {
-        afficherLePlateau(plateau);
-        erreur("\t   Vous n'avez pas le droit de faire ce déplacement");
-    }
+    do {
+        //On lui demande les coordonées où il veut aller
+        selectionnerUneCaseDeplacement(&caseDeplacement,joueur);
+
+        //Ici il y aura la vérification 
+        if (verificationDeDeplacement(plateau,caseSelectionne,caseDeplacement) == 1){
+            //On déplace le joueur 
+            plateau->tab[caseSelectionne.y - 1][caseSelectionne.x - 1] = 0;
+            plateau->tab[caseDeplacement.y - 1][caseDeplacement.x - 1] = joueur;
+            afficherLePlateau(plateau);
+        }else {
+            afficherLePlateau(plateau);
+            erreur("\t   Vous n'avez pas le droit de faire ce déplacement");
+        }
+    }while(!(verificationDeDeplacement(plateau,caseSelectionne,caseDeplacement)));
+
     return 0;
 }
 
@@ -57,10 +60,12 @@ int verificationDeDeplacement(Plateau *plateau,Case caseSelectionne,Case caseDep
     if (testDeCase(plateau,&caseDeplacement)){
         //Premiere vérification de déplacement simple
         if (distanceEntreDeuxPions <= sqrt(2)){
+            printf("OK");
             return 1;
         }
         //Deuxieme vérification de saut
         else if ((distanceEntreDeuxPions == sqrt(8) || distanceEntreDeuxPions == 2 )){
+            printf("OK");
             return 1;
         }
     }
