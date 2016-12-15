10b215ec13d8321fd364e7c6b091275f67991e52
Mathis Delaunay <delaunaymathis@yahoo.fr>
2016-12-15 09:34:36 +0100

    Indentation in sauvegarde.c

--- sauvegarde.c
+++ sauvegarde.c
@@ -4,71 +4,71 @@
 
 void sauvegarde(Plateau plateauDeJeu){
 
-extern int joueur;
-FILE* fichier = NULL;
-char tab[5] = {'h','a','l','m','a'};
+	extern int joueur;
+	FILE* fichier = NULL;
+	char tab[5] = {'h','a','l','m','a'};
 
-fichier = fopen(".sauvegarde.txt", "w");
-if(fichier != NULL)
-{
-
-
-	fprintf(fichier, "%d\n\n", joueur);
-	int i, j, k=0;
-	for(i = 0; i < TAILLE; i++)
+	fichier = fopen(".sauvegarde.txt", "w");
+	if(fichier != NULL)
 	{
-		for(j = 0; j<TAILLE; j++)
+
+
+		fprintf(fichier, "%d\n\n", joueur);
+		int i, j, k=0;
+		for(i = 0; i < TAILLE; i++)
 		{
-			fprintf(fichier,"%d",plateauDeJeu.tab[i][j]-48);
-			k++;
-			if(k == 5)
+			for(j = 0; j<TAILLE; j++)
 			{
-				k = 0;
+				fprintf(fichier,"%d",plateauDeJeu.tab[i][j]-48);
+				k++;
+				if(k == 5)
+				{
+					k = 0;
+				}
 			}
+			fprintf(fichier, "\n");
+		
 		}
-		fprintf(fichier, "\n");
-	
+		fclose(fichier);
 	}
-	fclose(fichier);
-}
-else{
-	printf("Impossible d'ouvrir le fichier");
-}
+	else{
+		printf("Impossible d'ouvrir le fichier");
+	}
 
 }
 
 void chargerSauvegarde(Plateau *plateauDeJeu)
 {
 
-extern int joueur;
-FILE* fichier = NULL;
-int retourALigne;
+	extern int joueur;
+	FILE* fichier = NULL;
+	int retourALigne;
 
-fichier = fopen(".sauvegarde.txt", "r");
+	fichier = fopen(".sauvegarde.txt", "r");
 
-if(fichier != NULL)
-{
-	joueur = fgetc(fichier)-48;
-	retourALigne = fgetc(fichier);
-	retourALigne = fgetc(fichier);
-	int i,j;
-	char tamp;
-	for(i = 0; i < TAILLE; i++)
+	if(fichier != NULL)
 	{
-		for(j = 0; j < TAILLE; j++)
+		joueur = fgetc(fichier)-48;
+		retourALigne = fgetc(fichier);
+		retourALigne = fgetc(fichier);
+		int i,j;
+		char tamp;
+		for(i = 0; i < TAILLE; i++)
 		{
-			plateauDeJeu->tab[i][j]=fgetc(fichier);
-		//	printf("%d", fgetc(fichier)-'0' );		
+			for(j = 0; j < TAILLE; j++)
+			{
+				plateauDeJeu->tab[i][j]=fgetc(fichier);
+			//	printf("%d", fgetc(fichier)-'0' );		
+			}
+	 	retourALigne=fgetc(fichier);
+		
 		}
- 	retourALigne=fgetc(fichier);
-	
+		fclose(fichier);
 	}
-	fclose(fichier);
-}
-else
-{
-	printf("Impossible d'ouvrir le fichier");
-}
+	else
+	{
+		printf("Impossible d'ouvrir le fichier");
+	}
 }
 
 
