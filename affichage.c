cb9a1eecc602fef17abadbbc9d471e3ff697bcc5
Mathis Delaunay <delaunaymathis@yahoo.fr>
2016-12-27 15:19:13 +0100

    Add some comments

--- affichage.c
+++ affichage.c
@@ -2,45 +2,59 @@
 //Fonction qui affiche le tableau a un moment : t
 
 void afficherLePlateau(Plateau *plateau){
+	//On récupere le joueur qui joue
     extern int joueur;
+    //On efface l'écran
     system("clear");
+    //On fait des tabulations
     printf("\t\t      ");
+    //Placement des numéros du dessus
     for (int i = 0;i <= 9;i++){
         printf(COLOR_JAUNE "%d   " COLOR_RESET,i);
     }
+    //Placement des barres du dessus 
     printf("\n\n\t\t      ");
     for (int i = 0;i <= 9;i++){
-
         printf(COLOR_JAUNE "|   " COLOR_RESET);
     }
-
+    //Placement de la premiere lettre
     printf(COLOR_JAUNE "\n\n\t\tA  -  " COLOR_RESET);
+    //Boucle des lignes
     for (int i = 0 ; i < TAILLE ; i++){
+    	//Boucle au niveau des colonnes
         for (int j = 0 ; j < TAILLE ; j++){
+        	//Si la case est 0, mettre 0 en blanc
             if (plateau->tab[i][j] == '0'){
                 printf(COLOR_BLANC "%c   " COLOR_RESET,plateau->tab[i][j]);
             }
+            //Sinon si la case est 1 mettre en cyan
             else if (plateau->tab[i][j] == '1'){
                 printf(COLOR_CYAN "%c   " COLOR_RESET,plateau->tab[i][j]);
             }
+            //Sinon si la case est 2 mettre en rouge
             else if (plateau->tab[i][j] == '2'){
                 printf(COLOR_ROUGE "%c   " COLOR_RESET,plateau->tab[i][j]);
             }
+            //Sinon si la case est A mettre en cyan et clignoter ( Veut dire que c'est la séléction du joueur )
             else if (plateau->tab[i][j] == 'A'){  //A est pour la séction du joueur 1
             	plateau->tab[i][j] = joueur + 48;
                 printf(COLOR_CYAN FAIRE_CLIGNOTER "%c   " COLOR_RESET,plateau->tab[i][j]);
             }
+            //Sinon si la case est B mettre en rouge et clignoter ( Veut dire que c'est la séléction du joueur )
             else if (plateau->tab[i][j] == 'B'){ //B pour la séléction du joueur 2
             	plateau->tab[i][j] = joueur + 48;
                 printf(COLOR_ROUGE FAIRE_CLIGNOTER "%c   " COLOR_RESET,plateau->tab[i][j]);
             }
+            //Sinon si la case est * mettre en vert ( possibilités )
             else if (plateau->tab[i][j] == '*'){ //pour les possibilités
                 printf(COLOR_VERT "%c   " COLOR_RESET,plateau->tab[i][j]);
             }
+            //Sinon si mettre la case en couleur par default
             else {
                 printf("%c   ",plateau->tab[i][j]);
             }
         }
+        //Evite de mettre en trop le "I - "
         if (i < TAILLE - 1){
             printf(COLOR_JAUNE "\n\n\t\t%c  -  " COLOR_RESET,i + 66);
         }
@@ -60,6 +74,7 @@ void afficherLeMessageDuMenu(){
     printf("\t  ###############################################################\n\n\n");        
 }
 
+//Fonction pour afficher une erreur
 void erreur(char Message[]){
     printf("%s",Message);
 }
--- mouvements.c
+++ mouvements.c
@@ -10,10 +10,10 @@ int deplacerUnPion(Plateau *plateau){ //x et y sont les coordonnées où déplac
 
     //Boucle de séléction principale ( Pour éviter que l'utilisateur se trompe ou donne une mauvaise case)
 
-    do {
+    do {//Boucle de séléction d
 
-        //Boucle de séléction d'un pion
-        do {
+        
+        do { //Premiere boucle pour voir si la séléction est correcte 
             if (faireUneSaisie(&caseSelectionne,joueur,0) != 0){
             	return 1;
             }
@@ -21,14 +21,17 @@ int deplacerUnPion(Plateau *plateau){ //x et y sont les coordonnées où déplac
             verificationSelection = verificationDeSelection(plateau,&caseSelectionne,joueur);
             // plateau->tab[caseSelectionne.y][caseSelectionne.x] = -joueur;
         }while (verificationSelection);
+
         if (joueur == 1){
         	plateau->tab[caseSelectionne.y][caseSelectionne.x] = 'A';
     	}else {
     		plateau->tab[caseSelectionne.y][caseSelectionne.x] = 'B';
     	}
 
+    	//Afficher les déplacements possibles
         indicationDeDeplacement(*plateau,caseSelectionne);
 
+        //Conversion en ACSII du joueur
         plateau->tab[caseSelectionne.y][caseSelectionne.x] = joueur + 48;
         //On lui demande les coordonées où il veut aller
         faireUneSaisie(&caseDeplacement,joueur,1);
