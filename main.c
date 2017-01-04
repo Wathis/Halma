154d512b283a07c52068d8f891fa7e237ae37d9e
Wathis <delaunaymathis@yahoo.fr>
2017-01-04 10:55:16 +0100

    Update main.c

--- main.c
+++ main.c
@@ -25,7 +25,7 @@ int main (void){
             initialisation(&plateauDeJeu,&caseOriginJoueur1,&caseOriginJoueur2,1,2);
     	}
     	else if(choix == 2){
-    		char *parties[10] = {"Sauvegarde Precedante","Sauvegarde Gagne","Sauvegarde double saut","0"};
+    		char *parties[10] = {"Sauvegarde Precedante","Sauvegarde Gagne","0"};
 		    //recupererPartie(parties, 10); //Nous n'avons pas reussi à faire marcher cette fonction
     		afficherLesParties(parties,10);
             scanf("%d",&partieChoisie);
